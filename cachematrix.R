028549ee3f426834d3faf4342d47363ebc9cac0e
Dustin Ezell <meatcomputer@gmail.com>
2015-01-24 14:49:15 -0600

    First Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a list which is used to cache the inverse of a matrix once computed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y){
+    x <<- y
+    s <<- NULL 
+  }
+  get <- function() x
+  setSolve <- function(solve) s <<- solve
+  getSolve <- function() s
+  list(set = set,
+       get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getSolve()
+        if(!is.null(s)) {
+          message("getting cached data")
+          s
+        }
+        temp <- x$get()
+        s <- solve(temp)
+        x$setSolve(s)
+        s       
 }
